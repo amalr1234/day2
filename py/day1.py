b7dfe893524175a834d91f8eab829837ce765065
amalr1234 <amalrumaisha@gmail.com>
2025-12-02 11:18:26 +0800

    first commit

---
+++ py/day1.py
@@ -0,0 +1,34 @@
+name = "amal"
+age = 24
+weight = 55.9
+is_female = True
+
+print(name)
+print (type(name))
+
+print (age)
+print (type(age))
+
+print(weight)
+print(type(weight))
+
+print (is_female)
+print ( type(is_female))
+
+Celsius=25
+F= Celsius*9/5 + 32
+print (F)
+
+Celsius=float(input("Insert temperature in Celsius "))
+F=Celsius*9/5 + 32
+print (F)
+
+sen=float(input("Masukkan nilai"))
+RM = sen/100
+print (RM)
+
+minutes=float(input("Masukkan tempoh masa"))
+hours= minutes/60
+print (f"{hours:.2f} hours")
+
+
